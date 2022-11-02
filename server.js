9d2dc49314c483f91ca49db1806f7d04c9615d52
Orlando <orlandozepeda20@gmail.com>
2022-11-02 00:34:59 -0700

    added to server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require("console.table");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
 
 //Connects to database
 const db = mysql.createConnection(
@@ -16,7 +15,60 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+db.connect(function (err) {
+  if(err) throw err;
+  questionsPrompt();
 });
+
+function questionsPrompt() {
+  inquirer.prompt({
+    type: "list",
+    name: "options",
+    message: "Pick an option below!",
+    choices: [
+      "View all employees",
+      "View all roles",
+      "View all departments",
+      "Add an employee",
+      "Add a role",
+      "Add a department",
+      "Cancel"
+    ]
+  })
+  .then((response) => {
+    const {choices} = response;
+
+    if (choices === "View all employees") {
+      displayEmployees();
+    }
+
+    if (choices === "View all roles") {
+      displayRoles();
+    }
+
+    if (choices === "View all departments") {
+      displayDepartments();
+    }
+
+    if (choices === "Add an employee") {
+      addEmployee();
+    }
+
+    if (choices === "Add a role") {
+      addRole();
+    }
+
+    if (choices === "Add a department") {
+      addDepartment();
+    }
+
+    if (choices === "Cancel") {
+      db.end()
+    };
+  });
+};
+
+function displayEmployees () {
+  console.log("Displaying all employees \n");
   
+}
